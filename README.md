0ee2a0255fae011d742721387b5f624aa7a2531b
AndreAmorim05 <js.andreamorim@gmail.com>
2021-10-03 10:30:48 -0300

    Ui clone

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# KivyMD Hot Reload Template
+# Nubank's UI Clone with Kivy and KivyMD
+
+This project made with Kivy and KivyMD, reproduce the home interface of the Nubank's mobile app, only with the content provided by the framework (it explain some diferences in the icons and fonts), but in the general it got very close to the original one.
+
+<p align="center">
+  <img src="data\nubank ui clone.gif" width="350" title="hover text">
+</p>
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from kivymd.uix.screen import MDScreen
+from kivy.core.window import Window
+from kivymd.uix.card import MDCard
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
 
+# resize the window
+Window.size = (375,667)
+
+# main screen of the application
 class Application(MDScreen):
     pass
+    
+class MainApp(MDApp):
+    def build(self):
+        # changes the primary pallete and set the color to Purple-700
+        self.theme_cls.primary_palette = 'Purple'
+        self.theme_cls.primary_hue = '700'
+        return Application()
 
+Builder.load_file('application.kv')
 
 if __name__ == "__main__":
-    from kivymd.app import MDApp
-    from kivy.lang import Builder
-
-    class MainApp(MDApp):
-        def build(self):
-            return Application()
-    
-    Builder.load_file('application.kv')
-
     MainApp().run()
